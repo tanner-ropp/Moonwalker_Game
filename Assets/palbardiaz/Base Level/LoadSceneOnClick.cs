85d4a46cc68f6577b8ca22e2981f2cb3ba488dc9
Pilar Albar Diaz <pilu.ad@hotmail.com>
2020-04-11 00:29:32 -0400

    beta_version

---
+++ Assets/palbardiaz/Base Level/LoadSceneOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnClick : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public void LoadLevelOne()
+    {
+        SceneManager.LoadScene("SampleLevel");
+    }
+
+    public void StartScreen()
+    {
+        SceneManager.LoadScene("StartScreen"); 
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
